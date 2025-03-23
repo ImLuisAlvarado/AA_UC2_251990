1f5d98ba5b55881b7c453e0685970f1838d9a302
ImLuisAlvarado <Luisalvarado00z22@gmail.com>
2025-03-22 20:24:04 -0700

    **Segundo commit**
    -	Se creó el método de ordenamiento de inserción y se realizaron
    	las respectivas pruebas de ejecución y de depuración.

--- src/main/java/mx/itson/algoritmos/aa_uc2_251990/Ordenamientos.java
+++ src/main/java/mx/itson/algoritmos/aa_uc2_251990/Ordenamientos.java
@@ -41,5 +41,39 @@ public static int[] ordenamientoBurbuja(int[] a) {
     // Retorno del array ordenado: 1 operación al final del algoritmo.
     return a;                                              
 }
-    
+    /**
+     * ImLuisAlvarado
+     * @param arr el arreglo con los datos a ordenar.
+     * @return el arreglo ordenado.
+     * 
+     * Complejidad temporal en el peor caso:
+     * - Ciclo externo: (n - 1) iteraciones.
+     * - Ciclo interno: (i) iteraciones en el peor caso, donde i es el índice del ciclo externo.
+     * - Operaciones por iteración del ciclo interno: 4 (1 comparación + 3 asignaciones).
+     * - Total de operaciones: 4 * SUM(i=1 to n-1) (i) = 2n(n - 1).
+     * - Complejidad temporal: O(n^2).
+     */
+    public static int[] insertionSort(int[] arr) {
+        // Ciclo externo: se ejecuta (n - 1) veces, donde n es el tamaño del array.
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            // Almacenar el valor actual (key) que se va a insertar en la posición correcta.
+            int key = arr[i];
+            // Inicializar j como el índice del elemento anterior a key.
+            int j = i - 1;
+
+            // Ciclo interno: mover los elementos del arreglo que son mayores que key
+            // a una posición adelante de su posición actual.
+            while (j >= 0 && arr[j] > key) {
+                // Asignación: mover el elemento arr[j] a arr[j + 1].
+                arr[j + 1] = arr[j];
+                j = j - 1; // Decrementar j para continuar comparando.
+            }
+            // Colocar key en su posición correcta.
+            arr[j + 1] = key; // 1 operación de asignación.
+        }
+        // Retorno del array ordenado: 1 operación al final del algoritmo.
+        return arr;                                              
+    }
 }
+
--- src/main/java/mx/itson/algoritmos/aa_uc2_251990/Pruebas.java
+++ src/main/java/mx/itson/algoritmos/aa_uc2_251990/Pruebas.java
@@ -4,6 +4,7 @@
 
 package mx.itson.algoritmos.aa_uc2_251990;
 import java.util.Arrays;
+import static mx.itson.algoritmos.aa_uc2_251990.Ordenamientos.insertionSort;
 import static mx.itson.algoritmos.aa_uc2_251990.Ordenamientos.ordenamientoBurbuja;
 /**
  *
@@ -12,24 +13,18 @@ import static mx.itson.algoritmos.aa_uc2_251990.Ordenamientos.ordenamientoBurbuj
 public class Pruebas {
 
     public static void main(String[] args) {
-       int mejorCaso[] = {1,2,3,4,5,6};
-       int casoAleatorio[] = {3,5,8,4,2,7};
-       int peorCaso[] = {6,5,4,3,2,1};
+        int[] peorCaso = {6, 5, 4, 3, 2, 1}; // Peor caso: arreglo ordenado en sentido inverso
+        System.out.println("Arreglo antes de ordenar:");
+        
     
-     System.out.println("Antes de ordenar:");
-        System.out.println(Arrays.toString(mejorCaso));
-        System.out.println(Arrays.toString(casoAleatorio));
+        System.out.println("Antes de ordenar:");
         System.out.println(Arrays.toString(peorCaso));
 
         // Ordenar los arrays
-        mejorCaso = ordenamientoBurbuja(mejorCaso);
-        casoAleatorio = ordenamientoBurbuja(casoAleatorio);
-        peorCaso = ordenamientoBurbuja(peorCaso);
+        peorCaso = insertionSort(peorCaso);
 
         // Imprimir arrays después de ordenar
         System.out.println("\nDespués de ordenar:");
-        System.out.println(Arrays.toString(mejorCaso));
-        System.out.println(Arrays.toString(casoAleatorio));
         System.out.println(Arrays.toString(peorCaso));
     
     }
